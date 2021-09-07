38183cbe75995532f87379f28148ec912d2542ac
ヒルミーサクティ <74033002+HilmySakti@users.noreply.github.com>
2021-09-07 12:00:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,33 +12,33 @@ function anunya(len) {
     return result.join('');
 }
 
-console.log(chalk.cyan(`${chalk.red('- - SANSEKAI - -')}\nAnime Picture\nCreated by Yusril\nFb : facebook.com/nikkixploit\n${chalk.red('- - SANSEKAI - -')}\n`))
+console.log(chalk.cyan(`${chalk.red('- - HILMYSAKTI - -')}\nAnime Picture\nCreated by Hilmy\nYT : https://youtube.com/c/HILMYGAMING87\n${chalk.red('- - HILMYSAKTI - -')}\n`))
 console.log(`${chalk.cyan('TYPE')}\n1. Wallpaper\n2. Waifu\n3. Waifu (NSFW)\n`)
 
-const type = readlineSync.questionInt(chalk.yellow("- Mau type nomor berapa?: "))
-if (type > 3) return console.log('Pilihannya cuma 1 - 3 gan')
+const type = readlineSync.questionInt(chalk.yellow("- Choose 1/2/3?: "))
+if (type > 3) return console.log('You Only Can Choose 1 - 3')
 
-const jml = readlineSync.questionInt(chalk.yellow("- Mau download berapa gambar? (max 50): "))
-if (jml > 50) return console.log('Maksimal 50 gan')
+const jml = readlineSync.questionInt(chalk.yellow("- How many image you want to download? (max 50): "))
+if (jml > 50) return console.log('Maximal 50!!')
 
 
 try {
-    console.log(chalk.yellow('\nLagi proses download...\nNanti letak gambarnya ada di folder "wp-anime"'))
+    console.log(chalk.yellow('\nDownloading...\n later the image will be in the "wp-anime" folder'))
     for (let i = 0; i < jml; i++) {
         if (type == '1') {
             axios.get(`https://nekos.life/api/v2/img/wallpaper`)
                 .then(async (res) => {
                     const data = res.data.url
-                    if (fs.existsSync('/sdcard/wp-anime')) {
+                    if (fs.existsSync('/sdcard/wp-anime/wallpaper')) {
                         const response = await fetch(data);
                         const buffer = await response.buffer();
-                        fs.writeFile(`/sdcard/wp-anime/${anunya(5)}.jpg`, buffer, () =>
+                        fs.writeFile(`/sdcard/wp-anime/wallpaper${anunya(5)}.jpg`, buffer, () =>
                             console.log(chalk.green('finished downloading!')))
                     } else {
-                        fs.mkdir('/sdcard/wp-anime')
+                        fs.mkdir('/sdcard/wp-anime/wallpaper')
                         const response = await fetch(data);
                         const buffer = await response.buffer();
-                        fs.writeFile(`/sdcard/wp-anime/${anunya(5)}.jpg`, buffer, () =>
+                        fs.writeFile(`/sdcard/wp-anime/wallpaper/${anunya(5)}.jpg`, buffer, () =>
                             console.log(chalk.green('finished downloading!')))
                     }
                 })
@@ -46,16 +46,16 @@ try {
             axios.get(`https://api.waifu.pics/sfw/waifu`)
                 .then(async (res) => {
                     const data = res.data.url
-                    if (fs.existsSync('/sdcard/wp-anime')) {
+                    if (fs.existsSync('/sdcard/wp-anime/waifu')) {
                         const response = await fetch(data);
                         const buffer = await response.buffer();
-                        fs.writeFile(`/sdcard/wp-anime/${anunya(5)}.jpg`, buffer, () =>
+                        fs.writeFile(`/sdcard/wp-anime/waifu${anunya(5)}.jpg`, buffer, () =>
                             console.log(chalk.green('finished downloading!')))
                     } else {
-                        fs.mkdir('/sdcard/wp-anime')
+                        fs.mkdir('/sdcard/wp-anime/waifu')
                         const response = await fetch(data);
                         const buffer = await response.buffer();
-                        fs.writeFile(`/sdcard/wp-anime/${anunya(5)}.jpg`, buffer, () =>
+                        fs.writeFile(`/sdcard/wp-anime//waifu${anunya(5)}.jpg`, buffer, () =>
                             console.log(chalk.green('finished downloading!')))
                     }
                 })
@@ -63,21 +63,21 @@ try {
             axios.get(`https://api.waifu.pics/nsfw/waifu`)
                 .then(async (res) => {
                     const data = res.data.url
-                    if (fs.existsSync('/sdcard/wp-anime')) {
+                    if (fs.existsSync('/sdcard/wp-anime/nsfw')) {
                         const response = await fetch(data);
                         const buffer = await response.buffer();
-                        fs.writeFile(`/sdcard/wp-anime/${anunya(5)}.jpg`, buffer, () =>
+                        fs.writeFile(`/sdcard/wp-anime//nsfw${anunya(5)}.jpg`, buffer, () =>
                             console.log(chalk.green('finished downloading!')))
                     } else {
-                        fs.mkdir('/sdcard/wp-anime')
+                        fs.mkdir('/sdcard/wp-anime/nsfw')
                         const response = await fetch(data);
                         const buffer = await response.buffer();
-                        fs.writeFile(`/sdcard/wp-anime/${anunya(5)}.jpg`, buffer, () =>
+                        fs.writeFile(`/sdcard/wp-anime//nsfw${anunya(5)}.jpg`, buffer, () =>
                             console.log(chalk.green('finished downloading!')))
                     }
                 })
         } else {
-            console.log(chalk.red('Pilihan hanya 1 - 3'))
+            console.log(chalk.red('only 3 choices'))
         }
     }
 } catch (err) {
